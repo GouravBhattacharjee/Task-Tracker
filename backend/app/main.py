f98e43848a9a469eb0b6d939befa62abe3cb8e06
gourav.bhattacha <gourav.bhattacha@tigeranalytics.com>
2025-08-06 11:46:13 +0530

    Added cloudfront URL to middleware CORS origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,7 +9,10 @@ app.include_router(router, prefix="/api")
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://d32hcqk0f0krzp.cloudfront.net"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
